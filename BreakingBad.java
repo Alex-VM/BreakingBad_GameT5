d1ec0a3fa13185dfc2f66f92649a40f32593d314
Alejandro Villaseñor Medina <opr.avm@hotmail.com>
2015-09-16 18:16:02 -0500

    Create BreakingBad.java

---
+++ BreakingBad.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Kevin
+ */
+
+import javax.swing.JFrame;
+
+public class BreakingBad extends JFrame{
+
+    public BreakingBad()
+    {
+        add(new Juego());
+        setTitle("Breaking Bad");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(Dimensiones.WIDTH, Dimensiones.HEIGTH);
+        setLocationRelativeTo(null);
+        setIgnoreRepaint(true);
+        setResizable(false);
+        setVisible(true);
+    }
+}
