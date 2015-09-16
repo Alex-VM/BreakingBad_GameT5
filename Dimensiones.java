112425cc2c118a349204d66e24e81be553d35730
Alejandro Villaseñor Medina <opr.avm@hotmail.com>
2015-09-16 18:15:42 -0500

    Create Dimensiones.java

---
+++ Dimensiones.java
@@ -0,0 +1,12 @@
+/*
+ * 
+ */
+
+
+public interface Dimensiones {  
+    public static final int WIDTH = 500;    //ancho
+    public static final int HEIGTH = 600;   //largo
+    public static final int BOTTOM = 580;   //Si llega aqui la bola pierdes
+    public static final int PLATAFORMA_RIGHT = 430; //Hasta donde puede llegar la plataforma
+    public static final int bola_RIGHT = 465;   //Hasta donde puede llegar la plataforma
+}
